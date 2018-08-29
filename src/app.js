9ddb27fb251bbab667ed7417d435d00e844172cf
Diego <diegotowerss@hotmail.com>
2018-08-29 14:30:09 +1000

    app with styiling done

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import './styles/styles.scss';
 import expenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
 
@@ -47,7 +48,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
